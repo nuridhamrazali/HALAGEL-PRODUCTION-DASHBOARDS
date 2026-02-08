c0bb9c77d515a82fe94a0393f6ad2226de5e8622
idham razali <nuridham.razali98@gmail.com>
2026-02-08 10:24:04 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -19,13 +19,13 @@ export const INITIAL_USERS: User[] = [
   { id: 'u1', name: 'Admin User', username: 'admin', email: 'admin@nexus.com', role: 'admin', password: 'password123' },
   { id: 'u1', name: 'Idham (Administrator)', username: 'Idham', email: 'admin@halagel.com', role: 'admin', password: 'admin123' },
   { id: 'u2', name: 'Hazlina (Manager)', username: 'hazlina', email: 'haz@halagel.com', role: 'manager', password: 'pass123' },
-   { id: 'u2', name: 'Ghazali (HOD)', username: 'manager', email: 'ghazali@halagel.com', role: 'manager', password: 'pass123' },
+   { id: 'u2', name: 'Ghazali (HOD)', username: 'ghazali', email: 'ghazali@halagel.com', role: 'manager', password: 'pass123' },
    { id: 'u2', name: 'Azura (SV)', username: 'manager', email: 'sv@halagel.com', role: 'manager', password: 'pass123' },
    { id: 'u2', name: 'Hafiz (Manager)', username: 'hafiz', email: 'hafiz@halagel.com', role: 'manager', password: 'pass123' },
   { id: 'u3', name: 'Planner User', username: 'planner', email: 'planner@nexus.com', role: 'planner', password: 'password123' },
   { id: 'u3', name: 'Umaira (Planner)', username: 'Umaira', email: 'umai@halagel.com', role: 'planner', password: 'pass123' },
   { id: 'u4', name: 'Operator User', username: 'operator', email: 'operator@nexus.com', role: 'operator', password: 'password123' },
-   { id: 'u4', name: 'Toothpaste (Operator)', username: 'operator', email: 'operator@halagel.com', role: 'operator', password: 'pass123' },
+   { id: 'u4', name: 'Toothpaste (Operator)', username: 'toothpaste', email: 'operator@halagel.com', role: 'operator', password: 'pass123' },
 ];
 
 export const INITIAL_OFF_DAYS: OffDay[] = [
